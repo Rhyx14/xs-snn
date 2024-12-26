a30033bd7e99a87be2122fa0142df84d1519e9de
x99 <xhr@x99>
2024-12-26 17:07:54 +0800

    bug fix

--- xs_snn/utils/spikingjelly/split_parameters.py
+++ xs_snn/utils/spikingjelly/split_parameters.py
@@ -8,31 +8,27 @@ def split_parameters_for_SNN(module):
     @return : decay, // including CONV, Linear,
             : no_decay, // including BN, bias of each layer
     '''
-
     params_decay = []
     params_no_decay = []
-    for m in module.modules():
-        if isinstance(m, torch.nn.Linear):
-            params_decay.append(m.weight)
-            if m.bias is not None:
-                params_no_decay.append(m.bias)
-        elif isinstance(m, torch.nn.modules.conv._ConvNd):
-            params_decay.append(m.weight)
-            if m.bias is not None:
-                params_no_decay.append(m.bias)
-        elif isinstance(m, torch.nn.modules.batchnorm._BatchNorm):
-            params_no_decay.extend([*m.parameters()])
-        elif isinstance(m, MemoryModule):
-            params_no_decay.extend([*m.parameters()])
+    for _m in module.modules():
+        if isinstance(_m, torch.nn.Linear):
+            params_decay.append(_m.weight)
+            if _m.bias is not None:
+                params_no_decay.append(_m.bias)
+        elif isinstance(_m, torch.nn.modules.conv._ConvNd):
+            params_decay.append(_m.weight)
+            if _m.bias is not None:
+                params_no_decay.append(_m.bias)
+        elif isinstance(_m, (torch.nn.modules.batchnorm._BatchNorm, MemoryModule)):
+            params_no_decay.extend([*_m.parameters()])
 
-        elif hasattr(m,'split_parameters'):
-            _decay,_no_decay=m.split_parameters()
+        elif hasattr(_m,'split_parameters'):
+            _decay,_no_decay=_m.split_parameters()
             params_decay.extend([*_decay])
             params_no_decay.extend([*_no_decay])
         else:
-            for _d in m.__dict__:
-                if isinstance(_d,torch.nn.Parameter):
-                    params_no_decay.append(_d)
+            for _2_param in _m._parameters.values():
+                params_no_decay.append(_2_param)
 
     assert len(list(module.parameters())) == len(params_decay) + len(params_no_decay)
     return params_decay, params_no_decay
