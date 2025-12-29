0af15af266ed2b464cad256443fee24372d497ba
msk <msk@h12d>
2025-12-30 00:24:47 +0800

    method rename

--- xs_snn/norm/Fuse_RateBatchNorm.py
+++ xs_snn/norm/Fuse_RateBatchNorm.py
@@ -7,19 +7,19 @@ import torch,inspect
 from xs_snn.utils.fuse_conv2d import fuse_conv2d
 
 @torch.no_grad()
-def fuse_rateBatchNorm_sj(module):
+def fuse_rateBatchNorm(module):
     if not isinstance(module,ASL_sj):
         return
     _layers=module._layer
     if not isinstance(_layers,(TimestepContainer,))  \
-        or (not isinstance(_layers[0],(torch.nn.Conv2d,torch.nn.ConvTranspose2d))) \
+        or (not isinstance(_layers._torch_module,(torch.nn.Conv2d,torch.nn.ConvTranspose2d))) \
         or (not isinstance(module._norm, (RateBatchNorm,BatchNorm))):
         return
     
-    _new_conv=fuse_conv2d(_layers[0],module._norm._norm)
+    _new_conv=fuse_conv2d(_layers._torch_module,module._norm._norm)
     module._layer= TimestepContainer(_new_conv)
     module._norm= Identical_Wrapper()
 
-def check_norm_sj(module):
+def check_norm(module):
     if isinstance(module,ASL_sj):
         assert isinstance(module._norm,Identical_Wrapper) or inspect.isfunction(module._norm)
--- xs_snn/norm/__init__.py
+++ xs_snn/norm/__init__.py
@@ -1,4 +1,4 @@
 from .RateBatchNorm import RateBatchNorm
-from .Fuse_RateBatchNorm import fuse_rateBatchNorm_sj,fuse_conv2d
+from .Fuse_RateBatchNorm import fuse_rateBatchNorm,fuse_conv2d
 from .TEBN import TEBN
 from .BatchNorm import BatchNorm
